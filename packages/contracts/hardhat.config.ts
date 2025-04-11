402cccff676ed94ae3dc65f39bcfd77625f690ff
Complexlity <nwalozielijah@gmail.com>
2025-04-11 19:07:20 +0100

    feat: start deploy and verify

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,6 +1,13 @@
 import "@nomicfoundation/hardhat-toolbox-viem";
-import { type HardhatUserConfig } from "hardhat/config";
+import { type HardhatUserConfig, vars } from "hardhat/config";
 import './tasks'
+
+
+const ETHERSCAN_API_KEY = vars.get('ETHERSCAN_API_KEY')
+const ALCHEMY_API_KEY = vars.get('ALCHEMY_API_KEY')
+const ARB_SEPOLIA_PRIVATE_KEY = vars.get('ARB_SEPOLIA_PRIVATE_KEY')
+const ARB_PRIVATE_KEY = vars.get('ARB_PRIVATE_KEY')
+const ARBISCAN_API_KEY = vars.get('ARBISCAN_API_KEY')
 
 
 
@@ -8,8 +15,28 @@ const config: HardhatUserConfig = {
   solidity: "0.8.28",
   paths: {
     sources: "./src",
-
+  }, 
+  sourcify: {
+    enabled: true,
+  },
+  networks: {
+    arbitrumSepolia: {
+      url: `https://arb-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [ARB_SEPOLIA_PRIVATE_KEY],
+    }, 
+    arbitrum: {
+      url: `https://arb-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [ARB_PRIVATE_KEY],
+    }, 
+  },
+  etherscan: {
+    apiKey: {
+      mainnet: ETHERSCAN_API_KEY,
+      arbitrumOne: ARBISCAN_API_KEY,
+      arbitrumSepolia : ARBISCAN_API_KEY,
+    }
   }
 };
-
+ 
 export default config;
+
